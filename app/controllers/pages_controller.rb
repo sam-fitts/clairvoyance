5e0b272f408fed34ae9aaadb268587deaa08c6b9
Sal Jim <saljimeely@chai-haleeb.com>
2018-03-15 08:55:22 +0400

    contact us page with backend and thumbs up picture

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :contact, :thank_you]
 
   def home
     @users = User.all
@@ -8,4 +8,13 @@ class PagesController < ApplicationController
   def profile
     @user = current_user
   end
+
+  def contact
+    @users = User.all
+  end
+
+  def thank_you
+    @users = User.all
+  end
+
 end
